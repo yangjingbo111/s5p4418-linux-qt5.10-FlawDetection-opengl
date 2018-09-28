ee2ae557d990d4c0a0f1caeb19358bc90ad75221
yangjingbo111 <yangjb111@gmail.com>
2018-09-27 19:02:19 -0700

    finish translations in menu 3

--- FlawDetection/res/translations/la_cn.ts
+++ FlawDetection/res/translations/la_cn.ts
@@ -4,147 +4,189 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../../qml/main.qml" line="17"/>
+        <location filename="../../qml/main.qml" line="18"/>
         <source>Flaw Detection</source>
         <translation>探伤仪</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="62"/>
+        <location filename="../../qml/main.qml" line="110"/>
         <source>Flaw Detection App</source>
         <translation>探伤仪</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="68"/>
+        <location filename="../../qml/main.qml" line="116"/>
         <source>exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="226"/>
+        <location filename="../../qml/main.qml" line="368"/>
         <source>menu_gain</source>
         <translation>基本增益(dB)</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="250"/>
+        <location filename="../../qml/main.qml" line="379"/>
         <source>menu_range</source>
         <translation>声程(mm)</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="270"/>
+        <location filename="../../qml/main.qml" line="390"/>
         <source>menu_high_vol</source>
         <translation>高压(V)</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="294"/>
+        <location filename="../../qml/main.qml" line="406"/>
         <source>menu_damp</source>
         <translation>发射阻尼</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="295"/>
+        <location filename="../../qml/main.qml" line="407"/>
         <source>low_damp</source>
         <translation>低阻尼</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="295"/>
+        <location filename="../../qml/main.qml" line="407"/>
         <source>high_damp</source>
         <translation>高阻尼</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="314"/>
+        <location filename="../../qml/main.qml" line="417"/>
         <source>menu_filter_band</source>
         <translation>滤波频带</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="346"/>
+        <location filename="../../qml/main.qml" line="441"/>
         <source>menu_rectificationType</source>
         <translation>检波方式</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="348"/>
+        <location filename="../../qml/main.qml" line="443"/>
         <source>rectification_radio</source>
         <translation>射频</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="349"/>
+        <location filename="../../qml/main.qml" line="444"/>
         <source>rectification_all</source>
         <translation>全波</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="350"/>
+        <location filename="../../qml/main.qml" line="445"/>
         <source>rectification_plus</source>
         <translation>正检波</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="351"/>
+        <location filename="../../qml/main.qml" line="446"/>
         <source>rectification_minus</source>
         <translation>负检波</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="362"/>
+        <location filename="../../qml/main.qml" line="457"/>
         <source>menu_transducerType</source>
         <translation>探头类型</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="364"/>
+        <location filename="../../qml/main.qml" line="459"/>
         <source>transducer_single</source>
         <translation>单晶</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="365"/>
+        <location filename="../../qml/main.qml" line="460"/>
         <source>transducer_double</source>
         <translation>双晶</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="377"/>
+        <location filename="../../qml/main.qml" line="472"/>
         <source>menu_tcgGain</source>
         <translation>增益补偿</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="379"/>
+        <location filename="../../qml/main.qml" line="474"/>
         <source>tcg_off</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="380"/>
+        <location filename="../../qml/main.qml" line="475"/>
         <source>tcg_on</source>
         <translation>使能</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="392"/>
+        <location filename="../../qml/main.qml" line="487"/>
         <source>menu_keyBoardBacklight</source>
         <translation>键盘背光</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="394"/>
+        <location filename="../../qml/main.qml" line="489"/>
         <source>light_off</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="395"/>
+        <location filename="../../qml/main.qml" line="490"/>
         <source>light_low</source>
         <translation>低亮</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="396"/>
+        <location filename="../../qml/main.qml" line="491"/>
         <source>light_mid</source>
         <translation>中亮</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="397"/>
+        <location filename="../../qml/main.qml" line="492"/>
         <source>light_high</source>
         <translation>高亮</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="409"/>
+        <location filename="../../qml/main.qml" line="504"/>
         <source>menu_vga</source>
         <translation>VGA显示</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="412"/>
+        <location filename="../../qml/main.qml" line="507"/>
         <source>vga_on</source>
         <translation>使能</translation>
     </message>
     <message>
-        <location filename="../../qml/main.qml" line="411"/>
+        <location filename="../../qml/main.qml" line="528"/>
+        <source>menu_repeatFreq</source>
+        <translation>重复频率</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="539"/>
+        <source>menu_hardwareDraw</source>
+        <translation>硬件绘图</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="541"/>
+        <location filename="../../qml/main.qml" line="569"/>
+        <source>off</source>
+        <translation>关</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="542"/>
+        <location filename="../../qml/main.qml" line="570"/>
+        <source>on</source>
+        <translation>开</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="553"/>
+        <source>menu_echoDisplayMode</source>
+        <translation>显示模式</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="555"/>
+        <source>hollow</source>
+        <translation>空心波</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="556"/>
+        <source>solid</source>
+        <translation>实心波</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="567"/>
+        <source>menu_echoFreeze</source>
+        <translation>回波冻结</translation>
+    </message>
+    <message>
+        <location filename="../../qml/main.qml" line="506"/>
         <source>vga_off</source>
         <translation>关闭</translation>
     </message>
